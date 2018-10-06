8b92247e1ae6f3a574c6fc0ec5a0e018d9123524
Silas Kaggwa <silakag@gmail.com>
2018-10-06 17:00:20 -0500

    cleanup

--- lecture04/reader_process.js
+++ lecture04/reader_process.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 process.on('message', myPath => {
 	console.log("path: ",myPath);
-	fs.readFile(path.join(__dirname, myPath), 'UTF-8', (err, data) => {
-		if(err){
-			data = err.message;
-		}
-		//console.log(data);
-		process.send(data);
+	const readStream = fs.createReadStream(path.join(__dirname, myPath), {encoding: 'utf8'});
+
+	readStream.on('data', chunk => {
+		process.send(chunk);
+	});
+	readStream.on('error', err => {
+		process.send(err.message);
 	});
 });
