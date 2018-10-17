01623a52d1fca1ed584b83537a1aa81e4d891653
Silas Kaggwa <silakag@gmail.com>
2018-10-16 22:10:33 -0500

    lecture11

--- lecture11/myapp/src/app/app.component.ts
+++ lecture11/myapp/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-counter></app-counter>
+    <div>
+      <input value="{{counter}}" (keyup)=setCounter($event.target.value)/>
+      <app-counter [counter] = "counter" (counterChange)=onCounter($event)></app-counter>
+    </div>
   `,
-  styles: []
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'myapp';
+  counter: number = 4;
+
+  onCounter($event){
+    this.counter = $event;
+  }
+
+  setCounter($event){
+    this.counter = $event;
+  }
 }
--- lecture11/myapp/src/app/counter.component.ts
+++ lecture11/myapp/src/app/counter.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   template: `
-    <div>
+    <span>
       <button (click)="decrease()">-</button>
       {{counterValue}}
       <button (click)="increase()">+</button>
-    </div>
+    </span>
   `,
-  styles: []
+  styles: ['span {float: right; border: 2px solid red; padding: 20px;}']
 })
 export class CounterComponent implements OnInit {
 
-  private counterValue;
+  @Input('counter') counterValue: number;
+  @Output() counterChange = new EventEmitter();
   constructor() { 
     this.counterValue = 0;
   }
 
   increase(){
     this.counterValue++;
+    this.counterChange.emit(this.counterValue);
+    return false;
   }
 
   decrease(){
     this.counterValue--;
+    this.counterChange.emit(this.counterValue);
+    return false;
   }
 
   ngOnInit() {
